9a6a8368336436889fb54f467888eca97a5ed1d8
Karthikeyan KC <karthikeyankc@live.com>
2016-07-09 14:21:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tiel
+A simple computer robot in Python that learns from the user and responds based on the input.
+## Description
+This is a fun project I do when I am bored. It doesn't use NLP yet. Therefore, it is a simple and trivial program, which strictly responds only if the user asks or chats with accurately trained set of sentences.
+## How do I train Tiel?
+Just run the script. A SQLite database `memory.db` will be created with a table with two fields to store all your inputs and the corresponding training responses as you train Tiel. You can either ask and train the robot or hack your way into to the database and add your own statement and response combinations.
+## Plans
+Supercharge Tiel with NLTK.
+Teach emotions (add more fields).
+Take over the world.
---
+++ tiel.py
@@ -0,0 +1,66 @@
+import sqlite3
+import random
+
+# Baby memories.
+init_memory = {"question":['I do not know how to respond to this. Teach me and I will remember it!', 'How do I respond to this? Train me!', 'I have no idea how to respond to this! Teach me, will you?']}
+
+# A simple cognitive(ish) structure for a robot(ish) program to listen, process, learn and speak.
+class Robot(object):
+	def __init__(self):
+		global init_memory
+		self.init_memory = init_memory
+	
+	def load_memory(self, user_voice):
+		global conn
+		conn = sqlite3.connect('memory.db')
+		global c
+		c = conn.cursor()
+		c.execute ('CREATE TABLE IF NOT EXISTS response_memory (user_voice, response)')
+		conn.commit()
+		response_memory = c.execute('SELECT user_voice, response FROM response_memory WHERE user_voice=?', (user_voice,)).fetchall()
+		return response_memory
+
+	def listen(self):
+		user_voice = raw_input("[You] ").lower()
+		self.process(user_voice)
+
+	def process(self, user_voice):
+		
+		def remember(user_voice):
+			thoughts = self.load_memory(user_voice)
+			if thoughts:
+				for u, r in thoughts:
+					user_data = u.lower()
+					response_data = r.capitalize()
+			else:
+				user_data = None
+				response_data = "Hey there..."
+
+			return user_data, response_data
+
+		def learn(user_voice):
+			if user_voice != None:
+				learned_response = raw_input("[Tiel] "+random.choice(init_memory['question'])+"\n").lower()
+				print "[System] Storing response...\n"
+				c.execute('INSERT INTO response_memory(user_voice, response) VALUES(?,?)', (user_voice, learned_response))
+				conn.commit()
+				self.listen()
+			else:
+				self.speak(response_data)
+
+		user_data, response_data = remember(user_voice)
+		if user_voice == user_data:
+			self.speak(response_data)
+		else:
+			learn(user_voice)
+
+	def speak(self, response_data):
+		goodbye = ['bye', 'see you later', 'bubye', 'goodbye' ]
+		print "[Tiel] %s" %response_data.capitalize()
+		if response_data in goodbye:
+			exit()
+		else:
+			self.listen()
+
+tiel = Robot() # Tiel was a mascot I once designed for swyde. The name just sparked out of thin air.
+tiel.listen()
